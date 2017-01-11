b30e0103702c2055444eb4b7105a601b9f0b5857
Xin,Gao <xin.gao@nicta.com.au>
2017-01-11 17:29:36 +1100

    add one more form obj

--- zoyoeec/core/zoyoeforms.py
+++ zoyoeec/core/zoyoeforms.py
@@ -16,7 +16,18 @@ class Config(forms.Form):
   type = forms.CharField(max_length = 20)
   content = forms.CharField(max_length = 1024)
   command = forms.CharField(max_length = 32)
-  
+
+class Item(forms.Form):
+  name = forms.CharField(max_length = 50)
+  refid = forms.CharField(max_length = 50)
+  price = forms.DecimalField(decimal_places = 2)
+  cost = forms.DecimalField(decimal_places = 2)
+  description = forms.CharField(max_length = 2048)
+  specification = forms.CharField(max_length = 1024)
+  category = forms.CharField(max_length = 50)
+  sndcategory = forms.CharField(max_length = 50)
+  ebaycategory = forms.CharField(max_length = 50)
+  disabled = forms.BooleanField(required = False)
 
 def fetchError(rf):
   return dict((key, rf.error_class.as_text(value)) for (key, value) in rf.errors.items())
